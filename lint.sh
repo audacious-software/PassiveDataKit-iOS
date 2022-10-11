a4b768419f1111e34aed7ea53906b8831bb4c812
Chris Karr <chris@audacious-software.com>
2022-10-11 11:34:46 -0500

    Build updates

--- lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/local/xctool/xctool.sh -scheme PassiveDataKit -reporter json-compilation-database:compile_commands.json clean build
-/usr/local/oclint/bin/oclint-json-compilation-database -e AFNetworking -e mixpanel-iphone
